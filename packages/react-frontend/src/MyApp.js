5e77321645371294de7829ebecb4ca85ea496572
bhillyard <bhillyar@calpoly.edu>
2024-01-18 06:11:00 -0800

    fogot prop parameter for table body

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -2,21 +2,17 @@ import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
-const [characters, setCharacters] = useState([]);
-
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    } // the rest of the data
-  ]);
-
+  const [characters, setCharacters] = useState([]);
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
     setCharacters(updated);
+  }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
   }
 
   return (
@@ -25,7 +21,7 @@ function MyApp() {
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
-      <Form />
+      <Form handleSubmit={updateList}/>
     </div>
   );
 }
--- packages/react-frontend/src/Table.js
+++ packages/react-frontend/src/Table.js
@@ -11,17 +11,17 @@ function TableHeader() {
   );
 }
 
-function TableBody() {
+function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
         <td>
-          <button onClick={() => props.removeCharacter(index)}>
-            Delete
-          </button>
-        </td>
+              <button onClick={() => props.removeCharacter(index)}>
+              Delete
+              </button>
+          </td>
       </tr>
     );
   }
@@ -38,8 +38,7 @@ function Table(props) {
     <table>
       <TableHeader />
       <TableBody
-        characterData={props.characterData}
-        removeCharacter={props.removeCharacter}
+        characterData={props.characterData} removeCharacter={props.removeCharacter}
       />
     </table>
   );
